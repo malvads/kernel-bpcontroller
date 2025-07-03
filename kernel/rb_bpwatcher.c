0a48afa4d411d34c8b7628775353f34b9cba83ba
Miguel Alvarez <malvarez@redborder.com>
2025-07-03 10:45:50 +0100

    Add top

--- kernel/rb_bpwatcher.c
+++ kernel/rb_bpwatcher.c
@@ -11,7 +11,16 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
+/*
+  This Kernel module hooks into the packet_notfier from kernel https://github.com/torvalds/linux/blob/master/net/packet/af_packet.c#L4234 using kprobe,
+  detects link events changes and when __LINK_STATE_NOCARRIER it triggers call to bpctl_kernel_ioctl_ptr to activate bypass on Silicom cards
+*/
 
 #include <linux/kernel.h>
 #include <linux/module.h>
